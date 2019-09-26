6693b1543845a9471067d11f5467c2db0867bd29
Kevin Moy <kevinmoy@berkeley.edu>
2019-09-25 22:11:54 -0700

    Possibly finished A.2

--- Mandelbrot.c
+++ Mandelbrot.c
@@ -35,8 +35,24 @@ This function calculates the Mandelbrot plot and stores the result in output.
 The number of pixels in the image is resolution * 2 + 1 in one row/column. It's a square image.
 Scale is the the distance between center and the top pixel in one dimension.
 */
-void Mandelbrot(double threshold, u_int64_t max_iterations, ComplexNumber* center, double scale, u_int64_t resolution, u_int64_t * output){
-    
-}
+void Mandelbrot(double threshold, u_int64_t max_iterations, ComplexNumber* center, double scale, u_int64_t resolution, u_int64_t * output)
+{
+	 u_int64_t lw = 2 * resolution + 1; //length
+	 double inc = scale/resolution; //2.5(i)
+	 u_int64_t i;
+	 u_int64_t x_coord, y_coord;
+	 u_int64_t center_x = floor(pow(lw,2), 2);
+	 u_int64_t center_y = floor(pow(lw,2), 2);
+	 u_int64_t dx;
+	 u_int64_t dy;
+	 ComplexNumber* pt = newComplexNumber(0,0);
 
+     for(i=0;i<pow(lw,2);i++){ //Construct corresponding point C per index.
+     	x_coord = floor(i,lw);
+     	y_coord = i % lw;
+     	dx = x_coord - center_x;
+     	dy = y_coord - center_y;
+     	pt = newComplexNumber(Re(center)+inc*dx, Im(center)+inc*dy);
+     	*(output+i) = MandelbrotIterations(max_iterations, pt, 2.0); 
+     }
 
