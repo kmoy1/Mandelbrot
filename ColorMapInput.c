60340a96a5e96ecaa915efe81663cbf411a09b81
Kevin Moy <kevinmoy@berkeley.edu>
2019-09-30 17:16:08 -0700

    TEST B

--- ColorMapInput.c
+++ ColorMapInput.c
@@ -33,7 +33,7 @@ uint8_t** FileToColorMap(char* colorfile, int* colorcount)
 	}
 
 	for(c=getc(cfp); c!=EOF; c=getc(cfp)){
-		if(c=='\n'){
+		if(c=='\n' || c == '\0'){
 			num_lines = num_lines+1;
 		}
 	}
@@ -51,7 +51,7 @@ uint8_t** FileToColorMap(char* colorfile, int* colorcount)
 	colorcount = &num_colors;
 	printf("Array length N=%d\n", N); //sanity check (N != num_colorss necessarily)
 	uint8_t** c_arr = (uint8_t**) malloc(num_colors * sizeof(uint8_t**));//malloc a length-N 2D array for colors. 
-	for(int i=0; i<num_colors+1; i++){
+	for(int i=0; i<num_colors; i++){
 		c_arr[i] = (uint8_t *) malloc(3*sizeof(uint8_t)); //allocate space for each 3-int group also
 		fscanf(cfp, "%d %d %d", &c1, &c2, &c3);//read a line.
 		printf("Adding: [%d %d %d]\n", c1,c2,c3); //sanity check 2
