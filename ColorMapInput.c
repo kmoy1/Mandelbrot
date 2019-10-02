07d6ce80385aadd0ec1b188cfa7c332f83ad9712
Kevin Moy <kevinmoy@berkeley.edu>
2019-10-01 19:41:34 -0700

    TEST

--- ColorMapInput.c
+++ ColorMapInput.c
@@ -33,7 +33,7 @@ uint8_t** FileToColorMap(char* colorfile, int* colorcount)
 	}
 
 	for(c=getc(cfp); c!=EOF; c=getc(cfp)){
-		if(c=='\n' || c == '\0'){
+		if(c=='\n'){
 			num_lines = num_lines+1;
 		}
 	}
@@ -48,14 +48,14 @@ uint8_t** FileToColorMap(char* colorfile, int* colorcount)
 	// 		break;
 	// 	}
 	// }
-	*colorcount = num_colors;
 	printf("Array length N=%d\n", N); //sanity check (N != num_colorss necessarily)
 	if(num_lines-1 < N){
 		printf("Number of lines too big: Terminating.\n");
 		fclose(cfp);
 		return NULL;
 	}
-	uint8_t** c_arr = (uint8_t**) malloc(num_colors * sizeof(uint8_t**));//malloc a length-N 2D array for colors. 
+	*colorcount = N; //N passes the validity tests. WE'll have N colors.
+	uint8_t** c_arr = (uint8_t**) malloc(N * sizeof(uint8_t**));//malloc a length-N 2D array for colors. 
 	for(int i=0; i<num_colors; i++){
 		c_arr[i] = (uint8_t *) malloc(3*sizeof(uint8_t)); //allocate space for each 3-int group also
 		fscanf(cfp, "%d %d %d", &c1, &c2, &c3);//read a line.
