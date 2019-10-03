3989cbab6fa0336a75f88395d96271d4bc51d445
Kevin Moy <kevinmoy@berkeley.edu>
2019-10-02 17:41:05 -0700

    MAYBE FINAL

--- MandelMovie.c
+++ MandelMovie.c
@@ -40,7 +40,7 @@ void MandelMovie(double threshold, u_int64_t max_iterations, ComplexNumber* cent
 /*For sz iterations in the image, with each image converted to a color, which is then written to fp*/
 void convertToColorAndWrite(u_int64_t* image, uint8_t** colormap, FILE* fp, u_int64_t sz, int num_colors){
 	u_int64_t pixel;
-	printf("P6 %lf %lf %lf\n", sqrt((double)(sz)), sqrt((double)(sz)), 255);//print header.
+	printf("P6 %d %ld %d\n", sqrt((double)(sz))+1e-9, sqrt((double)(sz))+1e-9, 255);//print header.
 
 	for(int i=0;i<sz;i++){
 		if(*(image+i)>num_colors){
